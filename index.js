e9ff05e2158617f2fa2a54a0cef9b2881383a3c6
codegram <omondihills02@gmail.com>
2023-02-09 22:23:01 +0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let times = document.getElementById("times");
 let toggle=document.querySelector(".line");
+let nav=document.querySelector(".menu");
+let links=document.querySelectorAll(".menu > li");
 
 
 
@@ -7,9 +9,18 @@ let toggle=document.querySelector(".line");
 // times.style.display = 'block';
 
 toggle.addEventListener('click', show)
-function show(){
-    toggle.style.backgroundColor='green';
-    times.style.display = 'block';  
+function show(){ 
+    toggle.style.display='none';
+    times.style.display = 'block'; 
+    nav.style.display='block';
+    // nav.style.position='absolute';
+    // nav.style.top='1rem';
+    // nav.style.left='77vw';
+    // nav.style.marginRight='0.5rem';
+    // nav.style.backgroundColor='grey';
+    // nav.style.paddingLeft='1rem';
+    // links.style.marginBottom='2rem';
+    // links.style.fontSize='30pt';
 }
 
 
