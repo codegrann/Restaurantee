6fc5be01282126cb41275ccce953bdf96a0ebb10
codegram <omondihills02@gmail.com>
2023-02-09 22:46:39 +0300

    added functionality to hide and show the nav on click

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ let links=document.querySelectorAll(".menu > li");
 // toggle.style.backgroundColor='green';
 // times.style.display = 'block';
 
-toggle.addEventListener('click', show)
+toggle.addEventListener('click', show);
+times.addEventListener('click', hide);
 function show(){ 
     toggle.style.display='none';
     times.style.display = 'block'; 
     nav.style.display='block';
     
+}
+function hide(){
+    nav.style.display='none';
+    toggle.style.display='block';
+    times.style.display='none';
 }
 
 
