2994b6441a446eb064daac0f06c082945ee7c50a
codegram <omondihills02@gmail.com>
2023-02-09 22:33:31 +0300

    index.js

--- index.js
+++ index.js
@@ -13,14 +13,7 @@ function show(){
     toggle.style.display='none';
     times.style.display = 'block'; 
     nav.style.display='block';
-    // nav.style.position='absolute';
-    // nav.style.top='1rem';
-    // nav.style.left='77vw';
-    // nav.style.marginRight='0.5rem';
-    // nav.style.backgroundColor='grey';
-    // nav.style.paddingLeft='1rem';
-    // links.style.marginBottom='2rem';
-    // links.style.fontSize='30pt';
+    
 }
 
 
