02bcfa4863c89ff30f5393c7cefd9379ff8473bc
codegram <omondihills02@gmail.com>
2023-02-12 11:14:11 +0300

    index.js

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ let nav=document.querySelector(".menu");
 let links=document.querySelectorAll(".menu > li");
 
 
-
-// toggle.style.backgroundColor='green';
-// times.style.display = 'block';
-
 toggle.addEventListener('click', show);
 times.addEventListener('click', hide);
 function show(){ 
     toggle.style.display='none';
     times.style.display = 'block'; 
-    nav.style.display='block';
-    
+    nav.style.display='block'; 
 }
 function hide(){
     nav.style.display='none';
